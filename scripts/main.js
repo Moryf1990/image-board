8590f66324ad3ee86582771fe19b29c890e210a9
Mory Friedman <friedmanmory2@gmail.com>
2015-12-11 16:36:23 -0600

    Completed All Required Todo Items

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,42 @@
 'use strict';
 
 $(document).ready(function() {
-	var $viewAd =$('#ad');
-	var $viewCaption =$('#caption');
-	var $viewImage =$('#image');
-	var $viewUrl =$('#url');
-	var $viewCancel =$('#cancel');
-	var $viewPlus =$('#plus');
-	var $viewSection =$('#section');
+	var viewAd =$('#ad');
+	var viewCaption =$('#caption');
+	var viewImage =$('#image');
+	var viewUrl =$('#url');
+	var viewCancel =$('#cancel');
+	var viewPlus =$('#plus');
+	var viewSection =$('#section');
 
 
 	var url ='http://tiyfe.herokuapp.com/collections/morris1f';
 
 
-	$viewAd.toggle();
-	$viewSection.show();
+	viewAd.toggle();
+	viewSection.show();
 
-	$viewPlus.click(function() {
-		$viewAd.toggle();
+	viewPlus.click(function() {
+		viewAd.toggle();
 
 	});
 
 	$.get(
-				url,
-				function(response){
+		url,
+		function(response){
+			response.forEach(function(result, index) {
+	            viewSection.append('<img src="'+result.url+'">' + '<div>'+result.caption+'</div>');
+			});
+		}
+	);
 
-					response.forEach(function(result, index) {
-                    $viewSection.append('<img src="'+result.url+'">' + '<div>'+result.caption+'</div>')
-				})
-			'json'
-		})
 
-
-	$viewAd.submit(function(e) {
-		e.preventDefault();
+	viewImage.click(function(e) {
+		e.preventDefault(); 
+		// $('cancel').prop('disabled', true);
 		
-		var newUrl =$viewUrl.val();
-		var newCaption =$viewCaption.val();
+		var newUrl =viewUrl.val();
+		var newCaption =viewCaption.val();
 
 		$.post(
 			url,
@@ -44,12 +44,13 @@ $(document).ready(function() {
 			function(result) {
 				console.log('input saved', result);
 				console.log('result'._id);
-				$viewSection.append('<img src="'+result.url+'">' + '<div>'+result.caption+'</div>');
-
+				viewSection.append('<img src="'+result.url+'">' + '<div>'+result.caption+'</div>');
+				viewUrl.val('');
+				viewCaption.val('');
 			},
 			'json'
 
-			); 
+		); 
 
 		/*$.post(
 			'http://tiyfe.herokuapp.com/collections/morrisf',
@@ -57,27 +58,27 @@ $(document).ready(function() {
 			function(result) {
 				console.log('input saved', result);
 				console.log('result'._id);
-				$viewSection.append('<div>'+result.message+'<div>');
+				viewSection.append('<div>'+result.message+'<div>');
 			},
 			'json'
 
 			);*/
 
-		$newUrl.click(function() {
-			$viewSection.html(' ');
-			$viewSection.show();
-			$.get(
-				url,
-				function(response){
+		// $newUrl.click(function() {
+		// 	viewSection.html(' ');
+		// 	viewSection.show();
+		// 	$.get(
+		// 		url,
+		// 		function(response){
+		// 			response.forEach(function(response, index) {
+  //                   	viewSection.append('<div>'+response.title+'</div><div>'+response.video+'</div><div>'+response.description+'</div>');
+		// 			});
+		// 			// 'json'
+		// 		}
+		// 	);
+		// });
 
-					response.forEach(function(response, index) {
-                    $viewSection.append('<div>'+response.title+'</div><div>'+response.video+'</div><div>'+response.description+'</div>')
-				})
-			'json'
-		})
-});
-
-});
+	});
 });
 
 
